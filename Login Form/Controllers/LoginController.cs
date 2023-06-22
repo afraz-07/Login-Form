dbaa4a688fceaa4a072d8387cb40ab540b02abe9
afraz-07 <mohammedafraz0007@gmail.com>
2023-06-21 22:28:38 -0700

    Add project files.

---
+++ Login Form/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Login_Form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_Form.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(LoginModel log)
+        {
+
+            if (log.Username != null && log.Password!= null)
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
